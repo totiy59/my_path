0cd4ab766db81681038236a7850d51edc63683ac
totiy59 <totiytotiy76@example.com>
2025-12-02 13:25:47 +0300

    first commit

---
+++ bin/myfirstprojectdart.dart
@@ -0,0 +1,5 @@
+import 'package:myfirstprojectdart/myfirstprojectdart.dart' as myfirstprojectdart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${myfirstprojectdart.calculate()}!');
+}
---
+++ test/myfirstprojectdart_test.dart
@@ -0,0 +1,8 @@
+import 'package:myfirstprojectdart/myfirstprojectdart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
